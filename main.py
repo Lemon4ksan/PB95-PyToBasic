0f239e93519a397b435925d0a336135f80e16e99
Lemon4ksan <senya20151718@gmail.com>
2024-08-19 20:56:41 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 import ast
-from src.statements import assign
-from src.expressions import call
+from src.utils import read_ast
 
 def main():
     with open('programm.py', 'r') as file:
         tree = ast.parse(file.read())
 
-    for obj in tree.body:
-
-        if isinstance(obj, ast.Assign):
-            for inst in assign(obj):
-                instructions_list.append(inst.upper())
-
-        if isinstance(obj, ast.Expr):
-            if isinstance(obj.value, ast.Call):
-                instructions_list.append(call(obj.value).upper())
+    read_ast(tree.body, instructions_list)
 
 if __name__ == '__main__':
     instructions_list = []
     main()
-    for index, instruction in enumerate(instructions_list):
-        print(f'{index + 1:02d} {instruction}')
+    for index, instruction in enumerate(instructions_list, start=1):
+        print(f'{index:02d} {instruction.upper()}')
