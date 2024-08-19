7eb895a08f962f29ce9703fe323f041a817b7b9b
Lemon4ksan <senya20151718@gmail.com>
2024-08-19 13:34:19 +0300

    File created

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+from .constants import SUPPORTED_OPERAIONS
+from .constants import BASE_OPERATIONS
+
+from .expressions import bin_op
+
+from .statements import assign
+
+__all__ = [
+    'SUPPORTED_OPERAIONS',
+    'BASE_OPERATIONS',
+    'bin_op',
+    'assign'
+]
