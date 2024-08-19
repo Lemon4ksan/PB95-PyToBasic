bebe0d31f38f5cce8af5d03c8a1a3dce64869bd2
Lemon4ksan <senya20151718@gmail.com>
2024-08-19 18:07:48 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,20 @@ from .constants import SUPPORTED_BINARY_OPERAIONS
 from .constants import BASE_OPERATIONS
 from .constants import SUPPORTED_FUNCTIONS
 
+from .aliases import color
+from .aliases import fill
+from .aliases import cls
+from .aliases import sin
+from .aliases import cos
+from .aliases import tan
+from .aliases import rnd
+from .aliases import background
+from .aliases import plot
+from .aliases import line
+from .aliases import circle
+
 from .expressions import bin_op
+from .expressions import call
 
 from .statements import assign
 
@@ -10,6 +23,18 @@ __all__ = [
     'SUPPORTED_BINARY_OPERAIONS',
     'BASE_OPERATIONS',
     'SUPPORTED_FUNCTIONS',
+    'color',
+    'fill',
+    'cls',
+    'sin',
+    'cos',
+    'tan',
+    'rnd',
+    'background',
+    'plot',
+    'line',
+    'circle',
     'bin_op',
+    'call',
     'assign'
 ]
