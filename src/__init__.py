e7ae2f7f2d3a70d5ac94da75093d5497dfa5ccc9
Lemon4ksan <senya20151718@gmail.com>
2024-08-19 16:00:17 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,15 @@
-from .constants import SUPPORTED_OPERAIONS
+from .constants import SUPPORTED_BINARY_OPERAIONS
 from .constants import BASE_OPERATIONS
+from .constants import SUPPORTED_FUNCTIONS
 
 from .expressions import bin_op
 
 from .statements import assign
 
 __all__ = [
-    'SUPPORTED_OPERAIONS',
+    'SUPPORTED_BINARY_OPERAIONS',
     'BASE_OPERATIONS',
+    'SUPPORTED_FUNCTIONS',
     'bin_op',
     'assign'
 ]
