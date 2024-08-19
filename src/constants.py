f6ebe06820332be0934c6a8631e3fe388acd5630
Lemon4ksan <senya20151718@gmail.com>
2024-08-19 15:59:01 +0300

    Added supported functions

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,8 @@
 import ast
 
-SUPPORTED_OPERAIONS = [ast.Add, ast.Sub, ast.Mult, ast.Div, ast.Mod, ast.Pow]
+SUPPORTED_BINARY_OPERAIONS = [ast.Add, ast.Sub, ast.Mult, ast.Div, ast.Mod, ast.Pow]
+
+SUPPORTED_FUNCTIONS = ['print']
 
 BASE_OPERATIONS = {
     ast.Add: '+',
