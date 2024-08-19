d24b7f56e7f4c23b92d5f798354f691b5db58be2
Lemon4ksan <senya20151718@gmail.com>
2024-08-19 18:06:46 +0300

    Added list of supported functions

--- src/constants.py
+++ src/constants.py
@@ -2,7 +2,21 @@ import ast
 
 SUPPORTED_BINARY_OPERAIONS = [ast.Add, ast.Sub, ast.Mult, ast.Div, ast.Mod, ast.Pow]
 
-SUPPORTED_FUNCTIONS = ['print']
+SUPPORTED_FUNCTIONS = [
+    'print',
+    'input',
+    'color',
+    'fill',
+    'cls',
+    'sin',
+    'cos',
+    'tan',
+    'rnd',
+    'background',
+    'plot',
+    'line',
+    'circle'
+]
 
 BASE_OPERATIONS = {
     ast.Add: '+',
