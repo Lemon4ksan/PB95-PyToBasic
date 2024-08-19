7bef81039414b4a1fa0b1f0b65c03619daa17e53
Lemon4ksan <senya20151718@gmail.com>
2024-08-19 21:20:14 +0300

    Revert "Updated main.py"
    This reverts commit 0f239e93519a397b435925d0a336135f80e16e99.

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import ast
-from src.utils import read_ast
+from src.statements import assign
+from src.expressions import call
 
 def main():
     with open('programm.py', 'r') as file:
         tree = ast.parse(file.read())
 
-    read_ast(tree.body, instructions_list)
+    for obj in tree.body:
+
+        if isinstance(obj, ast.Assign):
+            for inst in assign(obj):
+                instructions_list.append(inst.upper())
+
+        if isinstance(obj, ast.Expr):
+            if isinstance(obj.value, ast.Call):
+                instructions_list.append(call(obj.value).upper())
 
 if __name__ == '__main__':
     instructions_list = []
     main()
-    for index, instruction in enumerate(instructions_list, start=1):
-        print(f'{index:02d} {instruction.upper()}')
+    for index, instruction in enumerate(instructions_list):
+        print(f'{index + 1:02d} {instruction}')
